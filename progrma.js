ce316f1682bc75ca75e476157f2eb2b4aa3e222e
MatiOliverio <141592827+MatiOliverio@users.noreply.github.com>
2024-01-22 16:37:22 -0300

    Create progrma.js

---
+++ progrma.js
@@ -0,0 +1,9 @@
+// Función para mostrar el pop-up
+function mostrarPopUp() {
+    document.getElementById('miPopUp').style.display = 'block';
+}
+
+// Función para cerrar el pop-up
+function cerrarPopUp() {
+    document.getElementById('miPopUp').style.display = 'none';
+}
