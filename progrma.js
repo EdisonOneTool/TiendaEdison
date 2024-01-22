1f73375b567b685b6acafc133e023c073615e111
MatiOliverio <141592827+MatiOliverio@users.noreply.github.com>
2024-01-22 17:15:41 -0300

    Update progrma.js

--- progrma.js
+++ progrma.js
@@ -1,9 +1,1 @@
-// Función para mostrar el pop-up
-function mostrarPopUp() {
-    document.getElementById('miPopUp').style.display = 'block';
-}
 
-// Función para cerrar el pop-up
-function cerrarPopUp() {
-    document.getElementById('miPopUp').style.display = 'none';
-}
