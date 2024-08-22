0469221129e780068afaea8b9813a9aa27a5aa1a
bdgarat <garat.braian@gmail.com>
2024-08-22 14:38:37 -0300

    Cambio intercepto, pruebas extRef GP

--- functions.js
+++ functions.js
@@ -35,3 +35,19 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function interceptoConExtRef() {
+    if (QSI.API) {
+        hasInterceptLoaded=true;
+      } else {
+        hasInterceptLoaded=false;
+      }
+    console.log(hasInterceptLoaded)
+
+    if(typeof QSI === "unlimited") {
+        QSI = {};
+        QSI.config = {
+          externalReference: 152
+        };
+    }
+}
