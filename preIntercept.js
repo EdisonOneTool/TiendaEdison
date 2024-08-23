bc8c49e502b4dd27ee1a14f02262273a65772695
bdgarat <garat.braian@gmail.com>
2024-08-23 14:19:10 -0300

    cambiado userId

--- preIntercept.js
+++ preIntercept.js
@@ -1,8 +1,10 @@
 function preIntercept() {
     if(typeof QSI === "unlimited") {
+        userId = "152"
+        console.log(userId)
         QSI = {};
         QSI.config = {
-          externalReference: 152
+          externalReference: userId
         };
         console.log("cargado extRef")
     }
