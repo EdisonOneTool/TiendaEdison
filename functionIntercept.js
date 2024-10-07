58ba8da9a8ba603d9f3839891d14389c4151ef4b
bdgarat <garat.braian@gmail.com>
2024-10-07 11:46:20 -0300

    Inicio pruebas data embebida

--- functionIntercept.js
+++ functionIntercept.js
@@ -8,7 +8,10 @@ function interceptoConExtRef() {
 
     if (hasInterceptLoaded) {
         console.log("evaluando scripts")
-        QSI.API.run();
+        // QSI.API.run();
+        QSI.API.run('SI_6YDe0e3j19MQXz0', {}, {
+          "ApellidoNombre9": "Medvdev"  // Dato embebido que será enviado a Qualtrics
+        });
         console.log("script evaluado")
     } 
 }
