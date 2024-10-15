49371bee64ef94a1b95165dea7fdfa387389ba1c
bdgarat <garat.braian@gmail.com>
2024-10-15 11:46:15 -0300

    Prueba envio explicito data embebida

--- functionIntercept.js
+++ functionIntercept.js
@@ -11,8 +11,11 @@ function evaluarIntercepto() {
   if (hasInterceptLoaded) {
     var interceptId = 'SI_9vlIooAOHnLQGnc'
     console.log("Intercepto cargado. Evaluando script: " + interceptId)
-    var embebidoVariable = 'medvedev';
-    QSI.API.run(interceptId, {})
+    var embebidoVariable = 'HolaMundo';
+    // QSI.API.run(interceptId, {})
+    QSI.API.run('yourInterceptID', {}, {
+      "ApellidoNombre9": embebidoVariable  // Dato embebido que será enviado a Qualtrics
+    });
     console.log("Script evaluado. data embebida enviada: " + embebidoVariable)
   } else {
     console.log("No esta cargado el intercepto")
