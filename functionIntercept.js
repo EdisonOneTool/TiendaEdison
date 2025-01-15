8d6066ce45ec53405188b9d9cda5233246464721
bdgarat <garat.braian@gmail.com>
2025-01-15 15:18:14 -0300

    Prueba extRef + data embebida

--- functionIntercept.js
+++ functionIntercept.js
@@ -5,7 +5,7 @@ function interceptoConExtRef() {
       hasInterceptLoaded=false;
     }
   console.log("Se cargo el intercepto: " + hasInterceptLoaded)
-  sessionStorage.setItem("valorSesion", "Hola que tal");
+  sessionStorage.setItem("valorSesion", "Hola q tal");
   var data = sessionStorage.getItem("valorSesion");
   console.log("Valor de la sesion: " + data)
 }
@@ -13,17 +13,18 @@ function interceptoConExtRef() {
 function evaluarIntercepto() {
   if (hasInterceptLoaded) {
 
-    var interceptId = 'SI_bIvSOwnxmQdJ2zc' // SI_d5pdlVu5JyIGGjQ
+    // var interceptId = 'SI_bIvSOwnxmQdJ2zc' // SI_d5pdlVu5JyIGGjQ
 
-    console.log("Intercepto cargado. Evaluando script: " + interceptId)
+    // console.log("Intercepto cargado. Evaluando script: " + interceptId)
 
-    var idCliente = '1234';
+    // var idCliente = '1234';
 
+    QSI.API.run()
     // QSI.API.run(interceptId, {})
-    QSI.API.run(interceptId, {}, { "embebidoEnvio": idCliente });
+    // QSI.API.run(interceptId, {}, { "embebidoEnvio": idCliente });
 
 
-    console.log("Script evaluado. data embebida enviada: " + idCliente)
+    // console.log("Script evaluado. data embebida enviada: " + idCliente)
   } else {
     console.log("No esta cargado el intercepto")
   }
